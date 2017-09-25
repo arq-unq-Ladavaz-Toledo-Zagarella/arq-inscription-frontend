7acfa24616817b498141bfd7cdc3cf440f005b35
toledoJavier <toledojavier90@gmail.com>
2017-09-25 17:39:58 -0300

    added subjects at inscription view

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import { Router } from "@angular/router";
+import { environment } from "../../environments/environment"
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'inscription',
@@ -7,4 +11,15 @@ import { Component } from '@angular/core';
 })
 export class InscriptionComponent {
   
+  subjects= [{name: "dsada"}, {name:"ddd"}]
+  constructor(private http: Http, private router: Router) { 
+  	this.http.get(environment.apiEndpoint + '/subjects/list').map(res => res.json())
+		.subscribe(
+      result => {
+        console.log(result)
+        this.subjects= result
+        console.log(this.subjects)
+      },
+      error => { });
+	}
 }
